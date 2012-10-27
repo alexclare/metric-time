7860da0d6b0093a39acb5ddc5cdcfa43e119e249
Alexander Clare <alexander.clare@gmail.com>
2012-10-27 19:01:39 -0400

    Working/slightly more idiomatic animation code

--- src/metric-time.cljs
+++ src/metric-time.cljs
@@ -1,7 +1,15 @@
-(ns metric-time)
+(ns metric-time
+  (:require [goog.async.Delay :as delay]))
 
-(defn ^:export setup []
-  )
+(defn ^:export go [w h]
+  (let [n 1]
+    (js/p.size w h)
+    (js/p.background 255 255 255)
+    (draw 1)))
 
-(defn ^:export draw []
-  )
+(defn draw [n]
+  (js/p.fill 0 0 0)
+  (js/p.stroke 255 255 255)
+  (js/p.rect 0 0 js/p.width (* 20 n))
+  (doto (goog.async.Delay. #(draw (+ 1 n)) (* 1000 n))
+    (. start)))
