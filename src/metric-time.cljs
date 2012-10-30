a259d17e2955fa34ca9232f024ffb3c0b82dbd0c
Alexander Clare <alexander.clare@gmail.com>
2012-10-30 01:27:37 -0400

    Period notches

--- src/metric-time.cljs
+++ src/metric-time.cljs
@@ -3,33 +3,56 @@
             [goog.events :as events]))
 
 (def fps 60)
-(def background-color '(255 255 255))
-(def foreground-color '(0 0 0))
+(def background-color [255 255 255])
+(def foreground-color [0 0 0])
 
 (defn ^:export go [w h]
   (js/p.size w h)
-  (js/p.background 255 255 255)
+  (js/p.background background-color)
   (events/listen (doto (goog.Timer. (/ 1000 fps))
                    (. start))
                  goog.Timer/TICK draw))
+
+;;; Scale output (from 0 to 1) to size of canvas
+(defn scaled [[x y]]
+  [(* x js/p.width) (* y js/p.height)])
+
+;;; Draw and scale a two-point processing function
+(defn process [fn pa pb]
+  (apply fn (concat (scaled pa) (scaled pb))))b
+
+;;; Convert polar angle (in radians) to rectangular unit vector
+(defn p-to-r [phi]
+  [(js/p.sin phi) (js/p.cos phi)])
+
+;;; Convert a float between 0 and 1 to an angle in radians
+(defn angle [frac]
+  (let [pi 3.1415926535]
+    (* frac pi 2)))
 
 ;; Can certainly make the cleanup more efficient by only clearing dirty areas
 (defn cleanup []
   (apply js/p.fill background-color)
   (apply js/p.stroke background-color)
-  (js/p.rect 0 0 js/p.width js/p.height))
+  (process js/p.rect [0 0] [1 1]))
 
 (defn clock-base []
   (js/p.pushMatrix)
   (js/p.translate (/ js/p.width 2) (/ js/p.height 2))
   (js/p.noFill)
   (apply js/p.stroke foreground-color)
+
   ; Outline
   (js/p.strokeWeight 2)
-  (dotimes [i 2]
-    (js/p.ellipse 0 0
-                  (/ (* (+ i 1) js/p.width) 3)
-                  (/ (* (+ i 1) js/p.height) 3)))
+  (dotimes [i 2] (process js/p.ellipse [0 0] (repeat 2 (/ (+ i 1) 3))))
+
+  ; Notches (would look a bit cleaner if Ratio were supported in CLJS)
+  (js/p.strokeWeight 1)
+  (dotimes [i 100]
+    (let [[x y] (p-to-r (angle (/ i 100)))
+          start (if (= 0 (mod i 10)) (/ 9 30) (/ 3))
+          end (/ 11 30)]
+      (process js/p.line [(* x start) (* y start)] [(* x end) (* y end)])))
   (js/p.popMatrix))
 
 (defn draw []
